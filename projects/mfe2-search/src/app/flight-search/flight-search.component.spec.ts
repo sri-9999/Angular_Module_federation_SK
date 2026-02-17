7d37c139bd63e3e7f33611ffa3d3e4866bac5ffc
sri-9999 <sriharsha3445@gmail.com>
2026-02-17 21:19:39 +0530

    COMMIT 5

--- projects/mfe2-search/src/app/flight-search/flight-search.component.spec.ts
+++ projects/mfe2-search/src/app/flight-search/flight-search.component.spec.ts
@@ -5,19 +5,4 @@ import { FlightSearchComponent } from './flight-search.component';
 describe('FlightSearchComponent', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FlightSearchComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FlightSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+}
